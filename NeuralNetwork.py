a92a18e7dbc6bd003d3bc00b83ab9454adc1803d
Carlillous <carlosmenegg@gmail.com>
2023-11-14 17:49:44 +0000

    Added Backpro into NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Utils import loss, sigmoid
+from Utils import loss, sigmoid, sigmoid_prime, gradient_descent
 from Backpropagation import back_prop
 
 class NeuralNetwork:
@@ -21,7 +21,7 @@ class NeuralNetwork:
             for xi, yi in zip(x, Y):
                 out = self.__feed_forward(xi)
                 l.append(loss(out, yi))
-                self.w1, self.w2 = back_prop(xi, yi, self.w1, self.w2, alpha)
+                self.__back_prop(xi, yi, alpha)
             epoch_loss = sum(l) / len(x)
             epoch_acc = (1 - epoch_loss) * 100
             print(f"Epoch: {j + 1}, Accuracy: {epoch_acc:.2f}%")
@@ -47,3 +47,33 @@ class NeuralNetwork:
         z2 = a1.dot(self.w2)
         a2 = sigmoid(z2)
         return a2
+
+    def __back_prop(self, x, y, alpha):
+        # Forward pass
+        input_to_hidden = x.dot(self.w1)
+        hidden_activation = sigmoid(input_to_hidden)
+        hidden_to_output = hidden_activation.dot(self.w2)
+        output_activation = sigmoid(hidden_to_output)
+
+        # Compute the error at the output
+        derivative_output_error = output_activation - y
+
+        # Compute the derivative of the sigmoid function at the output layer
+        derivative_output_activation = sigmoid_prime(hidden_to_output)
+
+        # Compute the gradient for the output layer weights
+        gradient_w2_weights = hidden_activation.T.dot(derivative_output_activation * derivative_output_error)
+
+        # Compute the error at the hidden layer by backpropagating the output error through the weights
+        hidden_error = (derivative_output_error * derivative_output_activation).dot(self.w2.T)
+
+        # Compute the derivative of the sigmoid function at the hidden layer
+        derivative_hidden_activation = sigmoid_prime(input_to_hidden)
+
+        # Compute the gradient for the hidden layer weights
+        gradient_w1_weights = x.T.dot(derivative_hidden_activation * hidden_error)
+
+        # Update weights with the computed gradients
+
+        self.w1, self.w2 = gradient_descent(self.w1, self.w2, gradient_w1_weights, gradient_w2_weights, alpha)
+
