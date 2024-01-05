b49115423d1d33b252c4c828ddd969617ed478b4
Carlos <carlos.mendoza103@alu.ulpgc.es>
2024-01-05 12:00:24 +0000

    Added cross_entropy and NeuralNetwork refactoring.

---
+++ neural_network/LossFunctions.py
@@ -0,0 +1,27 @@
+import numpy as np
+from Activation import Sigmoid
+from Activation import Softmax
+
+class MSE():
+    def compute_loss(self,predicted, actual):
+        return np.mean((predicted - actual) ** 2)
+
+    def loss_derivative(self,predicted, actual):
+        return 2 * (predicted - actual) / actual.size
+
+
+class CrossEntropy():
+
+    def __init__(self,activation):
+        self._layer = activation
+
+    def compute_loss(self,predicted, actual):
+        if isinstance(self._layer,Sigmoid):
+            predicted = np.clip(predicted, 1e-15, 1 - 1e-15)  # Para estabilidad numérica
+            return -np.mean(actual * np.log(predicted) + (1 - actual) * np.log(1 - predicted))
+        else: #Si no, asumimos que es una softmax
+            predicted = np.clip(predicted, 1e-15, 1 - 1e-15)  # Para estabilidad numérica
+            return -np.mean(np.sum(actual * np.log(predicted), axis=1))
+
+    def loss_derivative(self,predicted, actual):
+        return predicted - actual
--- neural_network/NeuralNetwork.py
+++ neural_network/NeuralNetwork.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from LossFunctions import MSE, CrossEntropy
 from neural_network import DenseLayer
 
 
@@ -13,7 +13,7 @@ class NeuralNetwork:
             result = layer.forward(result)
         return result
 
-    def train(self, X, y, epochs, learning_rate):
+    def train(self, X, y, epochs, learning_rate,loss='mse'):
         self.__set_learning_rate(learning_rate)
         for epoch in range(epochs):
             # Forward pass
@@ -24,23 +24,29 @@ class NeuralNetwork:
                 activations.append(activation)
 
             # Backward pass
-            loss_derivative = self.loss_derivative(activations[-1], y)
+            loss_derivative = self.loss_derivative(activations[-1], y, loss)
             for i in reversed(range(len(self.layers))):
                 loss_derivative = self.layers[i].backward(activations[i], loss_derivative)
 
             # Registro del progreso
             if epoch % 10 == 0:
                 predicted = self.predict(X)
-                loss = self.compute_loss(predicted, y)
+                loss = self.compute_loss(predicted, y,loss,self.layers[-1])
                 print(f'Epoch {epoch}, Loss: {loss}')
 
     @staticmethod
-    def compute_loss(predicted, actual):
-        return np.mean((predicted - actual) ** 2)
+    def compute_loss(predicted, actual,loss,last_layer):
+        if loss == 'mse':
+            return MSE().compute_loss(predicted,actual)
+        else:
+            return CrossEntropy(last_layer).compute_loss(predicted,actual)
 
     @staticmethod
-    def loss_derivative(predicted, actual):
-        return 2 * (predicted - actual) / actual.size
+    def loss_derivative(predicted, actual,loss):
+        if loss == 'mse':
+            return MSE().loss_derivative(predicted, actual)
+        else:
+            return CrossEntropy().loss_derivative(predicted, actual)
 
     def __set_learning_rate(self,new_lr):
         for layer in self.layers:
